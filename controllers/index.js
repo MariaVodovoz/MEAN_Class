878795d2a14b207ce35ab5fa2c3b48a9cd8a8da9
Maria Vodovoz <mariiavodovoz@gmail.com>
2021-02-17 15:32:31 -0500

    first commit of portfolio project

--- controllers/index.js
+++ controllers/index.js
@@ -4,8 +4,8 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {
-    title: 'Project Tracker' ,
-    pageText: 'Here is some  dynamic  info from  the controller'
+    title: 'Portfolio' ,
+    pageText: 'some text - to be edited '
   });
 });
 
